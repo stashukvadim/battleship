b809205491e2c046b86f32b79b33142ff3b6b43d
Vadym Stashuk <stahukvadim@gmail.com>
2016-05-30 00:06:56 +0300

    Implement simple game controller using console input.

---
+++ src/main/java/game/GameController.java
@@ -0,0 +1,57 @@
+package game;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class GameController {
+    private Board board1;
+    private Board board2;
+    private Board whoseTurn;
+
+    public GameController() {
+        board1 = new Board();
+        board2 = new Board();
+        whoseTurn = board1;
+        board1.addShip(1, 1, true, 2);
+        board2.addShip(0, 0, 4);
+    }
+
+    public void fire(int x, int y) {
+        FireResult fire = whoseTurn.fire(x, y);
+        System.out.println(fire);
+        printBoards();
+        whoseTurn = whoseTurn == board1 ? board2 : board1;
+    }
+
+    public void startGame() {
+        Scanner scanner = new Scanner(System.in);
+        printBoards();
+        while (!gameOver()) {
+            try {
+                int x = scanner.nextInt();
+                int y = scanner.nextInt();
+                fire(x, y);
+            } catch (InputMismatchException e) {
+                System.out.println("Input is incorrect! Type x y with space between.");
+                scanner.nextLine();
+            }
+        }
+        System.out.println("Winner board is ");
+        System.out.println(getWinner());
+    }
+
+    private boolean gameOver() {
+        return board1.allShipsDead() || board2.allShipsDead();
+    }
+
+    private Board getWinner() {
+        return board1.allShipsDead() ? board2 : board1;
+    }
+
+    private void printBoards() {
+        System.out.println("Board1");
+        System.out.println(board1);
+        System.out.println("Board2");
+        System.out.println(board2);
+    }
+}

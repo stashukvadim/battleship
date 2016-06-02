8f697604ca5e823b28ba0a2be622fa44fd9546c5
Vadym Stashuk <stahukvadim@gmail.com>
2016-06-02 20:14:53 +0300

    Server: reformat ShipTest

--- src/test/java/com/stashuk/game/smartfox/battleship/model/ShipTest.java
+++ src/test/java/com/stashuk/game/smartfox/battleship/model/ShipTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.Collections;
 
 import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
@@ -57,5 +58,19 @@ public class ShipTest {
 
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> new Ship(asList(new Cell(0, 0), new Cell(2, 0))));
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenCreatingIllegalShip() {
+        new Ship(singletonList(new Cell(0, 0)));
+        new Ship(asList(new Cell(0, 0), new Cell(0, 1)));
+        new Ship(asList(new Cell(0, 0), new Cell(1, 0)));
+
+
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new Ship(emptyList()));
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> new Ship(asList(new Cell(0, 0), new Cell(0, 2))));
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> new Ship(asList(new Cell(0, 0), new Cell(0, 1), new Cell(1, 0))));
     }
 }
